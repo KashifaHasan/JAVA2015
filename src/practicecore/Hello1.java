c0847c278da710bdb5d02e695c7b630c802a6731
kashifa <kashifahasan27@gmail.com>
2015-08-02 14:19:45 -0500

    fc

---
+++ src/practicecore/Hello1.java
@@ -0,0 +1,12 @@
+package practicecore;
+
+public class Hello1 {
+	public static void main(String[] args){
+		System.out.println("Hello Java World");
+		System.out.println("public static void main is the main method that every class should have it");
+		System.out.println("Java is a language the same way like Ruby and Python");
+		System.out.println("Java Java");
+		
+	}
+
+}
